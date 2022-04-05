a3ea9c466690b1b060f3bd7f26ba049d3e6edadd
vahini01 <vahinimirididdi@gmail.com>
2022-04-05 12:32:10 +0530

    Add files

--- graphics/playground.cpp
+++ graphics/playground.cpp
@@ -8,6 +8,9 @@
 #include<GL/glew.h>
 #include<GLFW/glfw3.h>
 #include<glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <common/shader.hpp>
+
 
 using namespace glm;
 using namespace std;
@@ -72,7 +75,7 @@ int main() {
     // Open a window and create its OpenGL context
     GLFWwindow* window;
     window = glfwCreateWindow( 1024, 1024, "HP3 Assignment 2", NULL, NULL);
-    glEnable(GL_DEPTH_TEST);
+    
     
     if( window == NULL ){
         fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
@@ -84,18 +87,57 @@ int main() {
     
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
+        glfwTerminate();
         return -1;
     }
 
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     
+    // Dark Blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+    
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    
+    
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders( "SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader" );
+    
+    // Get a handle for our "MVP" uniform
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+    // Projection matrix : 45∞ Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    // Camera matrix
+    glm::mat4 View       = glm::lookAt(
+                                glm::vec3(4,3,-3), // Camera is at (4,3,-3), in World Space
+                                glm::vec3(0,0,0), // and looks at the origin
+                                glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+                           );
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model      = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+    
     do{
+        
+        // Clear the screen and use the shader program
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+        
 //        Dislay Hollow Cube
-//        drawCube(box);
+        drawCube(box);
         
 //        Display Spheres
-        drawSpheres();
+//        drawSpheres();
         
         // Swap buffers
         glfwSwapBuffers(window);
@@ -105,7 +147,6 @@ int main() {
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
            glfwWindowShouldClose(window) == 0 );
            
-      
     glfwTerminate();
     return 0;
 }
@@ -207,51 +248,113 @@ void drawCube(Container box) {
         // front face
         centerx - hLength, centery + hWidth, centerz + hDepth, // top left
         centerx+ hLength, centery + hWidth, centerz + hDepth, // top right
+        
+        centerx+ hLength, centery + hWidth, centerz + hDepth, // top right
+        centerx + hLength, centery - hWidth, centerz + hDepth, // bottom right
+        
         centerx + hLength, centery - hWidth, centerz + hDepth, // bottom right
         centerx - hLength, centery - hWidth, centerz + hDepth, // bottom left
+        
+        centerx - hLength, centery - hWidth, centerz + hDepth, // bottom left
+        centerx - hLength, centery + hWidth, centerz + hDepth, // top left
+        
         
         // back face
         centerx - hLength, centery + hWidth, centerz - hDepth, // top left
         centerx + hLength, centery + hWidth, centerz - hDepth, // top right
+        
+        centerx + hLength, centery + hWidth, centerz - hDepth, // top right
+        centerx + hLength, centery - hWidth, centerz - hDepth, // bottom right
+
         centerx + hLength, centery - hWidth, centerz - hDepth, // bottom right
         centerx - hLength, centery - hWidth, centerz - hDepth, // bottom left
+        
+        centerx - hLength, centery - hWidth, centerz - hDepth, // bottom left
+        centerx - hLength, centery + hWidth, centerz - hDepth, // top left
+
+        
         
         // left face
         centerx - hLength, centery + hWidth, centerz+ hDepth, // top left
         centerx - hLength, centery + hWidth, centerz - hDepth, // top right
+        
+        centerx - hLength, centery + hWidth, centerz - hDepth, // top right
+        centerx - hLength, centery - hWidth, centerz- hDepth, // bottom right
+        
+        
         centerx - hLength, centery - hWidth, centerz- hDepth, // bottom right
         centerx - hLength, centery - hWidth, centerz + hDepth, // bottom left
+        
+        centerx - hLength, centery - hWidth, centerz + hDepth, // bottom left
+        centerx - hLength, centery + hWidth, centerz+ hDepth, // top left
+
         
         // right face
         centerx + hLength, centery + hWidth, centerz + hDepth, // top left
         centerx + hLength, centery + hWidth, centerz - hDepth, // top right
+        
+        centerx + hLength, centery + hWidth, centerz - hDepth, // top right
+        centerx + hLength, centery - hWidth, centerz - hDepth, // bottom right
+        
         centerx + hLength, centery - hWidth, centerz - hDepth, // bottom right
         centerx + hLength, centery - hWidth, centerz + hDepth, // bottom left
+        
+        centerx + hLength, centery - hWidth, centerz + hDepth, // bottom left
+        centerx + hLength, centery + hWidth, centerz + hDepth, // top left
+
         
         // top face
         centerx - hLength, centery + hWidth, centerz + hDepth, // top left
         centerx - hLength, centery + hWidth, centerz - hDepth, // top right
+        
+        centerx - hLength, centery + hWidth, centerz - hDepth, // top right
+        centerx + hLength, centery + hWidth, centerz - hDepth, // bottom right
+        
         centerx + hLength, centery + hWidth, centerz - hDepth, // bottom right
         centerx + hLength, centery + hWidth, centerz + hDepth, // bottom left
+        
+        centerx + hLength, centery + hWidth, centerz + hDepth, // bottom left
+        centerx - hLength, centery + hWidth, centerz + hDepth, // top left
+
         
         // top face
         centerx - hLength, centery- hWidth, centerz+ hDepth, // top left
         centerx - hLength, centery - hWidth, centerz - hDepth, // top right
+        
+        centerx - hLength, centery - hWidth, centerz - hDepth, // top right
         centerx + hLength, centery - hWidth, centerz - hDepth, // bottom right
-        centerx + hLength, centery - hWidth, centerz + hDepth  // bottom left
+        
+        centerx + hLength, centery - hWidth, centerz - hDepth, // bottom right
+        centerx + hLength, centery - hWidth, centerz + hDepth,  // bottom left
+        
+        centerx + hLength, centery - hWidth, centerz + hDepth,  // bottom left
+        centerx - hLength, centery- hWidth, centerz+ hDepth, // top left
+        
     };
     
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
+    // 1rst attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+        0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        0,                  // stride
+        (void*)0            // array buffer offset
+    );
     
-    glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
-    glColor3f( 1, 1, 1 );
-    glEnableClientState( GL_VERTEX_ARRAY );
-    glVertexPointer( 3, GL_FLOAT, 0, vertices );
-
-    glDrawArrays( GL_LINES, 0, 24 );
+    // Draw the lines
+    glDrawArrays(GL_LINES, 0, 24);
+        
+    glDisableVertexAttribArray(0);
     
-    glDisableClientState( GL_VERTEX_ARRAY );
-    
+    glutWireCube()
 }
 
 void drawSpheres() {
